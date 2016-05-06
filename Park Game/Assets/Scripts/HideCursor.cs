df73155e2bfcbcfdbe8b11635941e615882033ca
Erick Navarro <Erick Navarro>
2016-05-06 11:29:59 -0700

    Added a pause menu with fake buttons and a simple continue button. New scene hides the mouse during gameplay and shows it again when the pause menu is brought up.

---
+++ Park Game/Assets/Scripts/HideCursor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideCursor : MonoBehaviour {
+
+    public bool show_cursor = false;
+	
+	void Awake ()
+    {
+        Cursor.visible = show_cursor;
+	}
+
+}
---
+++ Park Game/Assets/Scripts/UIManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI; //use UI elements (i.e. Sliders, Buttons, Text, ETC)
+using UnityStandardAssets.Characters.FirstPerson;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+    public GameObject pausePanel;
+    public GameObject FPSController;
+
+    public bool isPaused;
+
+	void Start ()
+    {
+        isPaused = false;
+	}
+	
+	void Update ()
+    {
+        PauseGame(isPaused); //Pauses the game based on the value of isPaused
+
+        if (Input.GetButtonDown("Cancel"))
+        {
+            SwitchPause();
+        }
+	}
+
+    void PauseGame(bool state)
+    {
+        if (state)
+        {
+            Time.timeScale = 0.0f; //Paused
+        }
+
+        else
+        {
+            Time.timeScale = 1.0f; //Unpaused
+        }
+        pausePanel.SetActive(state);
+        FPSController.GetComponent<FirstPersonController>().enabled = !state;
+        Cursor.visible = state;
+    }
+
+    public void SwitchPause()
+    {
+        if (isPaused)
+        {
+            isPaused = false;
+        }
+
+        else
+        {
+            isPaused = true;
+        }
+    }
+
+}
